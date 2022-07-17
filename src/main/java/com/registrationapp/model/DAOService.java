a85159cd2451c7a25726820e865e3b2b483fc059
Varshini D A <86996265+VarshiniDA@users.noreply.github.com>
2022-07-17 11:10:38 +0530

    Delete src/main directory

--- src/main/java/com/registrationapp/model/DAOService.java
+++
@@ -1,7 +0,0 @@
-package com.registrationapp.model;
-//Abstraction
-public interface DAOService {
-	public void connectDB();
-	public boolean verifyCredentials(String email,String password);
-	public void saveRegistration(String name, String city, String email, String mobile);
-}
